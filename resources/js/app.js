42785b8c23555a44cbbf4b6047a98ce1be139ddf
Jenish Prajapati <jenish.prajapati@easternts.com>
2023-10-10 15:42:45 +0530

    - Compress CSS with using Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
-import './bootstrap';
+//import './bootstrap';
+//import '/public/assets/plugins/global/plugins.bundle.js';
+//import '/public/assets/js/scripts.bundle.js';
 
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -22,32 +22,27 @@
         <!--begin::Fonts-->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
         <!--end::Fonts-->
-        <!--begin::Global Stylesheets Bundle(used by all pages)-->
-        <link href="{!! asset('assets/plugins/global/plugins.bundle.css') !!}" rel="stylesheet" type="text/css" />
-        <link href="{!! asset('assets/css/style.bundle.css" rel="stylesheet') !!}" type="text/css" />
-        <!--end::Global Stylesheets Bundle-->
+
         <!-- Scripts -->
         @livewireStyles
-        {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+        @vite(['resources/css/app.css','resources/js/app.js'])
 
     </head>
 
     <body id="kt_body" class="bg-body">
         {{ $slot }}
-
-        @livewireScripts
-        <!--end::Main-->
-        <script>var hostUrl = "assets/";</script>
         <!--begin::Javascript-->
-
+        @livewireScriptConfig
+        <script>var hostUrl = "assets/";</script>
         <!--begin::Global Javascript Bundle(used by all pages)-->
         <script src="{!! asset('assets/plugins/global/plugins.bundle.js') !!}"></script>
         <script src="{!! asset('assets/js/scripts.bundle.js') !!}"></script>
         <!--end::Global Javascript Bundle-->
+
         <!--begin::Page Custom Javascript(used by this page)-->
         <script src="{!! asset('assets/js/custom/authentication/sign-in/general.js') !!}"></script>
         <!--end::Page Custom Javascript-->
         <!--end::Javascript-->
     </body>
-
+    <!--end::Main-->
 </html>
