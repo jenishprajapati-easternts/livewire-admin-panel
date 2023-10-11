b194be8fdb27efba4d50e9a6c1a3187ff7c6bd55
Jenish Prajapati <jenish.prajapati@easternts.com>
2023-10-11 13:03:21 +0530

    - Create a components for login and getting text form lang folder

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -1,15 +1,36 @@
 <?php
 
 namespace App\Livewire\Auth;
-use Livewire\Attributes\Layout;
 
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 #[Layout('components.layouts.auth')]
+
 class Login extends Component
 {
+    #[Rule('required|string|max:255')]
+    public $email;
+    #[Rule('required|string|min:8|max:255')]
+    public $password;
+
     public function render()
     {
-        return view('livewire.auth.login');
+        return view('livewire.auth.login')->title(__('messages.login.title'));
+    }
+
+    public function login()
+    {
+        $this->validate();
+        $credentials = $this->only('email', 'password');
+        if (!Auth::attempt($credentials)) {
+
+            //return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+        }
+
+        //You have Successfully loggedin;
+
     }
 }
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -2,7 +2,8 @@
     <!--begin::Authentication - Sign-in -->
     <div class="d-flex flex-column flex-lg-row flex-column-fluid">
         <!--begin::Aside-->
-        <div class="d-flex flex-column flex-center flex-lg-row-auto w-xl-600px positon-xl-relative" style="background-color: #808080">
+        <div class="d-flex flex-column flex-center flex-lg-row-auto w-xl-600px positon-xl-relative"
+            style="background-color: #808080">
             <!--begin::Wrapper-->
             <div class="d-flex flex-column position-xl-fixed top-0 bottom-0 w-xl-600px scroll-y">
                 <!--begin::Content-->
@@ -13,10 +14,7 @@
                     </a>
                     <!--end::Logo-->
                     <!--begin::Title-->
-                     <!--begin::Description-->
-                     <p class="fw-bold fs-2" style="color: #ffffff;">Sparkle
-                    <!--end::Description-->
-                    <h1 class="fw-bolder fs-2qx pb-5 pb-md-10" style="color: #ffffff;">Central Admin</h1>
+                    <h1 class="fw-bolder fs-2qx pb-5 pb-md-10" style="color: #ffffff;">@lang('messages.login.heading_title')</h1>
                     <!--end::Title-->
                 </div>
                 <!--end::Content-->
@@ -31,49 +29,48 @@
                 <!--begin::Wrapper-->
                 <div class="w-lg-500px p-10 p-lg-15 mx-auto">
                     <!--begin::Form-->
-                    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="#">
+                    <form class="form w-100" wire:submit="login()">
                         <!--begin::Heading-->
                         <div class="text-center mb-10">
                             <!--begin::Title-->
-                            <h1 class="text-dark mb-3">Login</h1>
+                            <h1 class="text-dark mb-3">@lang('messages.login.title')</h1>
                             <!--end::Title-->
                         </div>
                         <!--begin::Heading-->
+
                         <!--begin::Input group-->
                         <div class="fv-row mb-10">
-                            <!--begin::Label-->
-                            <label class="form-label fs-6 fw-bolder text-dark">Email</label>
-                            <!--end::Label-->
-                            <!--begin::Input-->
-                            <input class="form-control form-control-lg form-control-solid" type="text" name="email" autocomplete="off" />
-                            <!--end::Input-->
+                            <x-label for="email" value="{{ __('messages.login.email') }}" />
+                            <x-input type="text" name="email" :value="old('email')" wire:model="email" autofocus autocomplete="email" />
+                            <x-input-error for="email" />
                         </div>
                         <!--end::Input group-->
+
                         <!--begin::Input group-->
                         <div class="fv-row mb-10">
-                            <!--begin::Wrapper-->
                             <div class="d-flex flex-stack mb-2">
-                                <!--begin::Label-->
-                                <label class="form-label fw-bolder text-dark fs-6 mb-0">Password</label>
-                                <!--end::Label-->
-                                <!--begin::Link-->
-                                <a href="../../demo1/dist/authentication/flows/aside/password-reset.html" class="link-primary fs-6 fw-bolder">Forgot Password ?</a>
-                                <!--end::Link-->
+                                <x-label for="password" value="{{ __('messages.login.password') }}" />
                             </div>
-                            <!--end::Wrapper-->
-                            <!--begin::Input-->
-                            <input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="off" />
-                            <!--end::Input-->
+                            <x-input type="password" name="password" wire:model="password" :value="old('password')" autocomplete="password" />
+                            <x-input-error for="password" />
                         </div>
                         <!--end::Input group-->
+
                         <!--begin::Actions-->
                         <div class="text-center">
                             <!--begin::Submit button-->
-                            <button type="submit" id="kt_sign_in_submit" class="btn btn-lg btn-primary w-100 mb-5">
-                                <span class="indicator-label">Continue</span>
+                            <x-button-primary class="btn-lg w-100 mb-5">
+                                {{ __('Next') }}
+                                <span class="svg-icon svg-icon-4 ms-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="black" />
+                                        <path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="black" />
+                                    </svg>
+                                </span>
                                 <span class="indicator-progress">Please wait...
-                                <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                            </button>
+                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                </span>
+                            </x-button-primary>
                             <!--end::Submit button-->
                         </div>
                         <!--end::Actions-->
